5e68286125ddf51fdffa2535f26737878d14be30
iftach barkay <43279785+ibarkay@users.noreply.github.com>
2019-07-29 00:11:27 +0300

    Update t1nba.py

--- t1nba.py
+++ t1nba.py
@@ -1,14 +1,16 @@
 import socket
 import requests
+import re
 
 #global serach vars
-ip = '212.149.50.184' #bank
+ip = '143.0.101.24' #bank
 localip = '10.0.2.146'
 time = '01:58:14'
 fily = open('list_of_conections','r')
 fily = fily.readlines()
 timeNumber = 205679
 list_of_c_after_bank = []
+port = 8080
 
 #creat i as  list form string
 
@@ -23,7 +25,8 @@ def creat_list(file):
         listy.append(i)
     return listy
 #############################
-def search_for_outIP(ip):    #of a bank ?
+
+def search_for_outIP(ip):    
     listof_matchIP = []
     for i in creat_list(fily):
         if str(ip) in str(i[3]):
@@ -42,11 +45,10 @@ def serch_for_LocalIP_after_c_with_ip(localip):
     list_of_local_out_after_bank = []
     for i in creat_list(fily):
         if localip in i[2]and timeNumber<= int(i[4]):
-            if len(list_of_local_out_after_bank) < 4:
+            if len(list_of_local_out_after_bank) < 4: #set how many
                 list_of_local_out_after_bank.append(i)
             else:
                 pass
-
     return list_of_local_out_after_bank
 
 def serch_for_LocalIP_BEFORE_c_with_ip(localip):
@@ -59,10 +61,6 @@ def serch_for_LocalIP_BEFORE_c_with_ip(localip):
     for i in range(4):
         list_of_local_out_before_bank.append(listmmm[-1])
         listmmm.remove(listmmm[-1])
-
-
-
-
     return list_of_local_out_before_bank[:-1]
 
 def Make_aList_conectedtwithIP(ip):
@@ -73,14 +71,37 @@ def Make_aList_conectedtwithIP(ip):
     return listing
 
 
-for i in serch_for_LocalIP_BEFORE_c_with_ip(localip):
-    print i
+def get_url(function):
+    urls = []
+    for i in function:
+        try:
+            urls.append(socket.gethostbyaddr(str(re.findall('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', i[3])[0])))
+        except:
+            print str(i)+' Error :cant get the URl'
+    for i in urls:
+        print i
 
+def check_port(listin):
 
+    global port
+    for i in listin:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(10)
+        ip = str(re.findall('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', i[3])[0])
+        print ip
+        result = sock.connect_ex((ip, port))
+        if result == 0:
+            print "Port is open"
+        else:
+            print "Port is not open"
+        sock.close()
 
+def send_POSTreq(listing):
+    pass
 
+def send_GETreq(listing):
+    pass
 
-
-
+#check_port(serch_for_LocalIP_BEFORE_c_with_ip(localip))
 
 
