1014be2c1759d3ca7387835821ba72066e59e0b0
iftach barkay <43279785+ibarkay@users.noreply.github.com>
2019-08-05 21:15:32 +0300

    Update MBA.py

--- MBA/MBA.py
+++ MBA/MBA.py
@@ -7,30 +7,52 @@ import base64
 fname = ''
 
 def read_msgB64():
+    listy = []
     for i in glob.glob('fs/mes*'):
         fname = str(i)
         fily = open(fname, 'r')
         fily = fily.read()
-        print fily.decode('base64'),
+        listy.append([fily.decode('base64'),fname])
+    return listy
+
+filysh = open('bin_try','wb')
+
 
 def reaf_binar():
-    for i in glob.glob('fs/bin_se*'):
-        fname = str(i)
+    for i in SderofBins:
+        fname = i
 
         with open(fname, "rb") as f:
             byte = f.read(1)
             while byte != "":
                 # Do stuff with byte.
                 byte = f.read(1)
-                #if byte.isalpha():
-                print byte,
-read_msgB64()
+                filysh.write(byte)
+#print read_msgB64()
+
+sentense = "One must divide one's time between politics and equations. But our equations are much more important to me, because politics is for the present, while our equations are for eternity."
+
+sentense = sentense.split(' ')
+#print sentense
+
+
+Seder = []
+for i in sentense:
+    for j in read_msgB64():
+        if j[0] == i :
+            Seder.append([j,int(sentense.index(i))])
+
+SderofBins = []
+for i in Seder:
+    SderofBins.append(i[0][1].replace('message','bin'))
+print SderofBins
+
+
+def Sort(sub_li):
+
+    sub_li.sort(key=lambda x: x[1])
+    return sub_li
+
 reaf_binar()
-
-
-
-
-
-
 
 
